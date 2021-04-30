ca99c716dd539de1dd642cd44a28102ea1d9a76b
Youngchae James Chee <litcoderr@gmail.com>
2021-04-30 14:09:51 +0900

    master: finished testing multidimensional matrix multiplication

--- src/main.cu
+++ src/main.cu
@@ -11,12 +11,12 @@ __global__ void cuda_mul(float* a, float* b, float* out, int dim) {
 
 int main() {
     // tensor 1
-    int shape_1[2] = {20, 30};
+    int shape_1[3] = {2, 10, 30};
     double matrix_1[600];
     for(int i=0; i<600; i++) {
         matrix_1[i] = 1;
     }
-    Tensor& t1 = *(new Tensor(2, shape_1, matrix_1));
+    Tensor& t1 = *(new Tensor(3, shape_1, matrix_1));
     
     // tensor 2
     int shape_2[2] = {30, 5};
@@ -29,12 +29,15 @@ int main() {
     // matrix multiplication
     Tensor& t3 = t1 * t2;
 
+    t1.print_shape();
     t1.print();
     std::cout << std::endl;
 
+    t2.print_shape();
     t2.print();
     std::cout << std::endl;
 
+    t3.print_shape();
     t3.print();
     std::cout << std::endl;
 
